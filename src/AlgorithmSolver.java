faec6baade79037ea34ebff855374f7edbe8327e
David <kido@seas.upenn.edu>
2015-01-20 16:49:16 -0500

    added AlgorithmSolver.java with main method

---
+++ src/AlgorithmSolver.java
@@ -0,0 +1,19 @@
+public class AlgorithmSolver {
+
+    SystemLoad myLoad;
+
+    // TODO: Define SystemLoad class. 
+    public AlgorithmSolver(SystemLoad load) {
+        myLoad = load;
+    }
+    
+    // TODO: Define Configuration class.
+    // solve() function should use the current state of the fleet (myLoad)
+    //  and return a Configuration object that gives the suggested configuration
+    //  for the fleet.
+    // If algorithm cannot solve given the load, 
+    //  then the method returns null and an appropriate log message is set.
+    public Configuration solve() {
+        
+    }
+}
