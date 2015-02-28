b4df868ab1ed8190b6debd01a32ce276d2f67b47
Alex Lyons <lyonsalexm@gmail.com>
2015-02-27 23:32:53 -0500

    hardware cluster interface

--- src/orchestration/HardwareClusterInterface.java
+++ src/orchestration/HardwareClusterInterface.java
@@ -1,16 +1,20 @@
 package orchestration;
 
-import java.util.HashMap;
 import java.rmi.*;
 /**
  * Interface for the Hardware Cluster to communicate with the State Manager.
- * ??? method ???
- * ??? method ???
+ * reconfigure method reorganizes the hardware cluster based on components
+ * specified by the State Manager.
  */
 
 public interface HardwareClusterInterface {
 
-    public void reconfigure(HashMap<Integer, Double> links, HashMap<Integer, Double> linkUtilization, HashMap<Integer, Double> machineUtilization);
+    public void bootVM(VM vm);
+    public void shutdownVM(VM vm);
+    public void startService(VM vm, RunningService runningservice);
+    public void terminateRunningService(VM vm, RunningService runningservice);
+    public void transferRunningService(VM old, VM new, RunningService runningservice);
 
-//    public Cloud getConfiguration(total workload) throws IllegalStateException;
+    // Networking and routing methods
+
 }
