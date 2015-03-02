d5f6f2c6e1ef97fb93573f815401a7846d45b84d
Alex Brashear <alexjbrashear@gmail.com>
2015-03-01 23:15:27 -0500

    junit working

--- src/test/ExampleTest.java
+++ src/test/ExampleTest.java
@@ -1,15 +1,28 @@
 package test;
 
-import org.junit.Test;
-import org.junit.Ignore;
-import org.junit.runner.RunWith;
-import org.junit.runners.JUnit4;
+import org.junit.*;
+import static org.junit.Assert.*;
+
+import orchestration.StateManager;
+import orchestration.Request;
+import orchestration.CustomerResponse;
+
 
 
 public class ExampleTest {
 
 	@Test
-	public void example() {
+	public void exampleTest1() {
+		StateManager sm = new StateManager();
+		Request request = new Request("r4","s3_0","s3_1",72432,44,"s0-s1-s3",12000,1);
+        CustomerResponse cr = StateManager.queryAlgorithmSolver(request);
+        System.out.println(cr.accepted);
+        assertTrue(cr.accepted);
+	}
+
+	@Test
+	public void exampleTest2() {
+
 	}
 
 }
