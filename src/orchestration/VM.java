44243f10d76f9be2d3e2564549c4811629c51d34
Alex Lyons <lyonsalexm@gmail.com>
2015-03-26 01:17:42 -0400

    changes

--- src/orchestration/VM.java
+++ src/orchestration/VM.java
@@ -12,12 +12,12 @@ import java.util.UUID;
 
 public class VM extends Node implements Serializable {
 	private String id = UUID.randomUUID().toString(); // uuid as string
-	double coresAllocated;	// VMs can request fractions of cores.
-	int memoryAllocated; 	// MB of memory allocated
-	String ipAddress;		// Get the IP address after booting
-	String status; 			// Booting, Running, Stopped, Crashed, Terminated
+	private double coresAllocated;	// VMs can request fractions of cores.
+	private int memoryAllocated; 	// MB of memory allocated
+	private String ipAddress;		// Get the IP address after booting
+	private String status; 			// Booting, Running, Stopped, Crashed, Terminated
 	// HashSet<Integer> serviceInstanceIDs = new HashSet<Integer>();
-	HashSet<ServiceInstance> serviceInstances = new HashSet<ServiceInstance>();
+	private HashSet<ServiceInstance> serviceInstances = new HashSet<ServiceInstance>();
 
 	public VM(VM other) {
 		id = other.getID();
@@ -38,4 +38,9 @@ public class VM extends Node implements Serializable {
 	public String getID() {
 		return id;
 	}
+
+	public int getMemory() {
+		return memoryAllocated;
+	}
+
 }
