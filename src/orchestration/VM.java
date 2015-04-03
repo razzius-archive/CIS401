cf155368033f261dea8150be8199a49ec7ff44bc
Alex Lyons <lyonsalexm@gmail.com>
2015-04-03 01:01:20 -0400

    UUID changes

--- src/orchestration/VM.java
+++ src/orchestration/VM.java
@@ -16,22 +16,9 @@ public class VM extends Node implements Serializable {
 	private int memoryAllocated; 	// MB of memory allocated
 	private String ipAddress;		// Get the IP address after booting
 	private String status; 			// Booting, Running, Stopped, Crashed, Terminated
-	// HashSet<Integer> serviceInstanceIDs = new HashSet<Integer>();
 	private HashMap<String, ServiceInstance> serviceInstances = new HashMap<String, ServiceInstance>();
 
-	public VM(VM other) {
-		id = other.getID();
-		coresAllocated = other.coresAllocated;
-		memoryAllocated = other.memoryAllocated;
-		ipAddress = other.ipAddress;
-		status = other.status;
-		for (String otherServiceInstanceID : other.serviceInstances.keySet()) {
-			serviceInstances.put(otherServiceInstanceID, new ServiceInstance(other.getServiceInstances().get(otherServiceInstanceID)));
-		}
-	}
-
 	public VM(double coresAllocated, int memoryAllocated) {
-		id = "1";
 		this.coresAllocated = coresAllocated;
 		this.memoryAllocated = memoryAllocated;
 	}
