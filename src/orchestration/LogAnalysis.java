a973844a54985d783c584f4edd4b9e2ab97063b1
Alex Lyons <lyonsalexm@gmail.com>
2015-04-14 19:00:19 -0400

    changes

--- src/orchestration/LogAnalysis.java
+++ src/orchestration/LogAnalysis.java
@@ -5,12 +5,15 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
+import java.io.PrintWriter;
+import java.io.UnsupportedEncodingException;
 
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 
 import java.util.ArrayList;
 import java.util.Date;
+import java.util.HashMap;
 
 import org.apache.log4j.Logger;
 
@@ -62,6 +65,74 @@ public class LogAnalysis {
 
 	public ArrayList<Date> getPacketsOut() {
 		return packetsOut;
+	}
+
+		public boolean allPacketsMadeIt() {
+		if (packetsIn.size() == packetsOut.size()) return true;
+		else return false;
+	}
+	
+	public long calculateMaxLatency() {
+		if (!allPacketsMadeIt()) return -1;
+		long lastPacketSent = packetsOut.get(packetsOut.size() - 1).getTime();
+		long lastPacketReceived = packetsIn.get(packetsIn.size() - 1).getTime();
+		return (lastPacketReceived - lastPacketSent);
+	}
+	
+	public double calculateAverageLatency() {
+		if (!allPacketsMadeIt()) return -1;
+		double sumLatency = 0;
+		for (int i = 0; i < packetsIn.size(); i++) {
+			long packetSent = packetsOut.get(i).getTime();
+			long packetReceived = packetsIn.get(i).getTime();
+			sumLatency += (packetReceived - packetSent);
+		}
+		double numDatapoints = new Double(packetsIn.size());
+		return (sumLatency/numDatapoints);
+	}
+	
+	public void generateLatencyScatterGraph(String fileName) {
+		if (!allPacketsMadeIt()) return;
+		PrintWriter writer;
+		try {
+			writer = new PrintWriter(fileName, "UTF-8");
+			for (int i = 0; i < packetsIn.size(); i++) {
+				long packetSent = packetsOut.get(i).getTime();
+				long packetReceived = packetsIn.get(i).getTime();
+				long latency = packetReceived - packetSent;
+				writer.println(i + "," + latency);
+			}
+			writer.close();
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		} catch (UnsupportedEncodingException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public void generateLatencyFrequencyGraph(String fileName) {
+		if (!allPacketsMadeIt()) return;
+		HashMap<Long, Integer> latencies = new HashMap<Long, Integer>();
+		for (int i = 0; i < packetsIn.size(); i++) {
+			long packetSent = packetsOut.get(i).getTime();
+			long packetReceived = packetsIn.get(i).getTime();
+			long latency = packetReceived - packetSent;
+			if (latencies.containsKey(latency)) {
+				latencies.put(latency, 1 + latencies.get(latency));
+			} else latencies.put(latency, 1);
+		}
+		PrintWriter writer;
+		try {
+			writer = new PrintWriter(fileName, "UTF-8");
+			for (Long l : latencies.keySet()) {
+				writer.println(l + "," + latencies.get(l));
+			}
+			writer.close();
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		} catch (UnsupportedEncodingException e) {
+			e.printStackTrace();
+		}
 	}
 
 }
