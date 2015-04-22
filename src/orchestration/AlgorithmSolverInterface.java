8721e03e65d86c63cf540ff0c0d3bc1231bead5c
Alex Lyons <lyonsalexm@gmail.com>
2015-04-21 20:24:20 -0400

    added ASI Javadoc

--- src/orchestration/AlgorithmSolverInterface.java
+++ src/orchestration/AlgorithmSolverInterface.java
@@ -10,10 +10,25 @@ import orchestration.State;
  * Interface for the Algorithm Solver to be queried by the StateManager.
  */
 
+/**
+ * @author      Dong Young Kim, Alex Brashear, Alex Lyons, Razzi Abuissa
+ * @version     1.0
+ * @since       2015-04-21
+ */
+
 public interface AlgorithmSolverInterface {
     /**
-     * Solves for a configuration that fulfills the specification.
-     * If none can be produced, returns null.
+     * Solves for a network configuration that fulfills the customer specification.
+     * 
+     * @param links         a set of all the links in the network
+     * @param switches      a set of all the switches in the network
+     * @param services      a set of all the services already running on the network hardware
+     * @param currentState  the current State of the deployment, incorporating running RemoteHosts and ServiceChains
+     * @param request       the request object constructed from the customer input
+     * @return              a list of modifications to the state needed to achieve the specification, or null if none is available.
+     * @see orchestration.RemoteHost
+     * @see orchestration.ServiceChain
+     * @see orchestration.Request
      */
     public ArrayList<Action> solve(
         Set<Link> links,
